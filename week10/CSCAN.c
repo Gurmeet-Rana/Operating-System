97a769eaad74145b152855ad3bde7113b156e56b
Gurmeet-Rana <ranagur361@gmail.com>
2025-11-12 18:59:51 +0530

    C-SCAN

--- week10/CSCAN.c
+++ week10/CSCAN.c
@@ -1,17 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int n;
-    printf("Enter the number of Requests : \n");
-    scanf("%d",&n);
-    int tracks[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&tracks[i]);
-    }
-    int totalSeekMovement=0;
-    printf("Total Seek Movement : %d\n",totalSeekMovement);
-
-    return 0;
+#include <stdio.h> 
+#include <stdlib.h> 
+int main() { 
+    int n, i, j, head, temp, total = 0, disk_size; 
+    printf("Sample I/O for C-SCAN:\n\nInput:\n"); 
+    printf("Enter number of disk requests : "); 
+    scanf("%d", &n); 
+    int req[n + 1]; 
+    for (i = 0; i < n; i++) 
+        scanf("%d", &req[i]); 
+    printf("\nEnter initial head position : "); 
+    scanf("%d", &head); 
+    printf("Enter total disk size : "); 
+    scanf("%d", &disk_size); 
+    req[n] = head; 
+    for (i = 0; i <= n; i++) { 
+        for (j = i + 1; j <= n; j++) { 
+            if (req[i] > req[j]) { 
+                temp = req[i]; 
+                req[i] = req[j]; 
+                req[j] = temp; 
+            } 
+        } 
+    } 
+    int pos; 
+    for (i = 0; i <= n; i++) { 
+        if (req[i] == head) { 
+            pos = i; 
+            break; 
+        } 
+    } 
+    for (i = pos; i < n; i++) 
+        total += abs(req[i + 1] - req[i]); 
+    total += (disk_size - 1 - req[n]) + (disk_size - 1); 
+    for (i = 0; i < pos; i++) 
+        total += abs(req[i + 1] - req[i]); 
+    printf("\nOutput:\nTotal seek movement : %d\n", total); 
 }
