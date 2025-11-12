9a9e8fd65be7c004a4cb88438a718c77354c2eea
Gurmeet-Rana <ranagur361@gmail.com>
2025-11-12 18:59:09 +0530

    SCAN

--- week10/FCFS.c
+++ week10/FCFS.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int n;
-    printf("Enter the number of Requests : \n");
-    scanf("%d",&n);
-    int tracks[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&tracks[i]);
-    }
-    int totalSeekMovement=0;
-    int prev=0;
-
-    for(int i=1;i<n;i++)
-    {
-        totalSeekMovement+=abs(tracks[i]-tracks[prev]);
-        prev=i;
-    }
-
-    printf("Total Seek Movement : %d\n",totalSeekMovement);
-
-    return 0;
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+int main() { 
+    int n, head, total = 0; 
+    printf("--- FCFS Disk Scheduling ---\n"); 
+    printf("Enter number of requests: "); 
+    scanf("%d", &n); 
+    int request[n]; 
+    printf("Enter request sequence: "); 
+    for (int i = 0; i < n; i++) 
+        scanf("%d", &request[i]); 
+ 
+    printf("Enter initial head position: "); 
+    scanf("%d", &head); 
+    printf("\nHead Movement Sequence: %d", head); 
+    for (int i = 0; i < n; i++) { 
+        total += abs(request[i] - head); 
+        head = request[i]; 
+        printf(" -> %d", head); 
+    } 
+    printf("\nTotal Head Movement: %d\n", total); 
+    printf("Average Seek Time: %.2f\n", (float)total / n); 
+    return 0; 
 }
--- week10/SCAN.c
+++ week10/SCAN.c
@@ -1,50 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
-int Partition(int arr[],int low,int high)
-{
-    int key=arr[high];
-    int i=low-1;
-    int j=low;
-    while(j<high)
-    {
-        if(arr[j]<key)
-        {
-            i++;
-            swap(arr[i],arr[j]);
-        }
-        j++;
-    }
-    i++;
-    swap(arr[i],arr[high]);
-    return i+1;
-}
-void quickSort(int arr[],int low,int high)
-{
-    if(low<high)
-    {
-        int pivot=Partition(arr,low,high);
-        quickSort(arr,low,pivot-1);
-        quickSort(arr,pivot+1,high);
-    }
-}
-int main()
-{
-    int n;
-    printf("Enter the number of Requests : \n");
-    scanf("%d",&n);
-    int tracks[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&tracks[i]);
-    }
-    int initial_pos=tracks[0];
-    quickSort(tracks,0,n-1);
-    int totalSeekMovement=0;
-    totalSeekMovement+=abs(tracks[n-1]-initial_pos);
-    totalSeekMovement+=abs(tracks[0]-initial_pos);
-    
-
-    printf("Total Seek Movement : %d\n",totalSeekMovement);
-
-    return 0;
-}
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+int main() { 
+    int n, i, j, temp, head = 50, total = 0; 
+    printf("Sample I/O for SCAN:\n\nInput:\n"); 
+    printf("Enter number of disk requests : "); 
+    scanf("%d", &n); 
+ 
+    int req[n + 1]; 
+    for (i = 0; i < n; i++) 
+        scanf("%d", &req[i]); 
+ 
+    req[n] = head;  
+    for (i = 0; i <= n; i++) { 
+        for (j = i + 1; j <= n; j++) { 
+            if (req[i] > req[j]) { 
+                temp = req[i]; 
+                req[i] = req[j]; 
+                req[j] = temp; 
+            } 
+        } 
+    } 
+    int pos; 
+    for (i = 0; i <= n; i++) { 
+        if (req[i] == head) { 
+            pos = i; 
+            break; 
+        } 
+    } 
+   for (i = pos; i < n; i++) 
+        total += abs(req[i + 1] - req[i]); 
+   for (i = pos - 1; i >= 0; i--) 
+        total += abs(req[i + 1] - req[i]); 
+} 
