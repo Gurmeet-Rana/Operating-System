99b526b221d390a056d22b5b537ab37d03be5675
Gurmeet-Rana <ranagur361@gmail.com>
2025-11-12 18:56:17 +0530

    Sequential Disk Sch. Algo

--- Week9/seq.c
+++ Week9/seq.c
@@ -1,22 +1,33 @@
-#include<stdio.h>
-#include<stdbool.h>
-typedef struct file 
-{
-    char name;
-    int start_block;
-    int no_of_blocks;
-}file;
-int main()
-{
-    bool block[1000]={true};
-    int n;
-    printf("Enter the number of files : \n");
-    scanf("%d",&n);
-    file files[n];
-    for(int i=0;i<n;i++)
-    {
-        getchar();
-
-    }
+#include <stdio.h> 
+#include <string.h> 
+ 
+struct File { 
+    char name[20]; 
+    int start, length; 
+}; 
+ 
+int main() { 
+    struct File f[10]; 
+    int n; 
+    printf("Enter number of files: "); 
+    scanf("%d", &n); 
+ 
+    for (int i = 0; i < n; i++) { 
+        printf("\nEnter file %d name: ", i + 1); 
+        scanf("%s", f[i].name); 
+        printf("Enter starting block of file %d: ", i + 1); 
+        scanf("%d", &f[i].start); 
+        printf("Enter number of blocks in file %d: ", i + 1); 
+        scanf("%d", &f[i].length); 
+    } 
+ 
+    printf("\n%-10s%-15s%-15s%-20s\n", "File Name", "Start block", "No. of blocks", "Blocks occupied"); 
+    for (int i = 0; i < n; i++) { 
+        printf("%-10s%-15d%-15d", f[i].name, f[i].start, f[i].length); 
+        for (int j = 0; j < f[i].length; j++) { 
+            printf("%d ", f[i].start + j); 
+        } 
+        printf("\n"); 
+    } 
+    return 0; 
 }
-
--- week10/SCAN.c
+++ week10/SCAN.c
@@ -1,5 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+int Partition(int arr[],int low,int high)
+{
+    int key=arr[high];
+    int i=low-1;
+    int j=low;
+    while(j<high)
+    {
+        if(arr[j]<key)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+        j++;
+    }
+    i++;
+    swap(arr[i],arr[high]);
+    return i+1;
+}
+void quickSort(int arr[],int low,int high)
+{
+    if(low<high)
+    {
+        int pivot=Partition(arr,low,high);
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+    }
+}
 int main()
 {
     int n;
@@ -10,37 +37,13 @@ int main()
     {
         scanf("%d",&tracks[i]);
     }
+    int initial_pos=tracks[0];
+    quickSort(tracks,0,n-1);
     int totalSeekMovement=0;
-    int prev=0;
+    totalSeekMovement+=abs(tracks[n-1]-initial_pos);
+    totalSeekMovement+=abs(tracks[0]-initial_pos);
+    
 
-    int large=(tracks[1]>tracks[0])?1:0;
-    for(int i=1;i<n;i++)
-    {
-        if(large && tracks[i]>tracks[prev])
-        {
-            totalSeekMovement+=abs(tracks[i]-tracks[prev]);
-           // prev=i;
-        }
-        else if (!large && tracks[i]<tracks[prev])
-        {
-            totalSeekMovement+=abs(tracks[i]-tracks[prev]);
-            //prev=i;
-        }
-    }
-    large=!large;
-    for(int i=0;i<n;i++)
-    {
-        if(large && tracks[i]>tracks[prev])
-        {
-            totalSeekMovement+=abs(tracks[i]-tracks[prev]);
-            //prev=i;
-        }
-        else if (!large && tracks[i]<tracks[prev])
-        {
-            totalSeekMovement+=abs(tracks[i]-tracks[prev]);
-           // prev=i;
-        }
-    }
     printf("Total Seek Movement : %d\n",totalSeekMovement);
 
     return 0;
